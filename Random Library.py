3bde7513108dcab5a3bd905d7929c41218fb32ff
Dev_elio <32529318+DevXL247@users.noreply.github.com>
2019-11-03 11:34:48 -0500

    Update Random Library.py

--- Random Library.py
+++ Random Library.py
@@ -8,11 +8,11 @@ def roll (sides):
 
 
 def mechanism():
-    sides = input("Enter the highest integer in your data range:")
     default_rolls = 0
     norolls = 0
 
     try:
+        sides = input("Enter the highest integer in your data range:")
         norolls = input("How many terms would you like in your sequence?")
     except Exception:
         print("Your input was not an integer. Please type in an applicable number.")
